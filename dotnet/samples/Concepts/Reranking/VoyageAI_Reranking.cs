2b8aaa6361345087fb44ba6bbae1878376616577
fzowl <zoltan@voyageai.com>
2026-09-10 20:09:01 +0200

    Fix dotnet format: add UTF-8 BOM and drop unused using in VoyageAI samples

--- dotnet/samples/Concepts/Reranking/VoyageAI_Reranking.cs
+++ dotnet/samples/Concepts/Reranking/VoyageAI_Reranking.cs
@@ -1,6 +1,5 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 
-using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.VoyageAI;
 
 namespace Reranking;
